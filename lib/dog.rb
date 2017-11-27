42bb9baa9512dce6161b2134883c96b5ea7c3c6b
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-27 15:55:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,16 +2,16 @@ class Dog
   def name=(inst_name)
     @name = inst_name
   end
-  
+
  def name
    @name
  end
 
 def breed(inst_breed)
-@breed = inst_breed
+  @breed = inst_breed
 end
 
  def breed
- @breed
+   @breed
  end
 end
